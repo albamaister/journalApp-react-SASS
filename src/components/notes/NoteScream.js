5725d3bac15f4494892ad8f29cf6abd4cc3e65d7
Andres Alba <bryanalba92@gmail.com>
2020-11-04 23:05:59 -0500

    Designed NoteScreem

--- src/components/notes/NoteScream.js
+++ src/components/notes/NoteScream.js
@@ -5,6 +5,13 @@ export const NoteScream = () => {
     return (
         <div className='notes__main-content'>
             <NotesAppBar/>
+            <div className='notes__content'>
+                <input type='text' placeholder='Some awesome title' className='notes__title-input' autoComplete='off'/>
+                <textarea placeholder='What happened today' className='notes__textarea'></textarea>
+                <div className='notes__image'>
+                    <img src="https://ep01.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572453030_noticia_normal.jpg" alt="imagen"/>
+                </div>
+            </div>
         </div>
     )
 }
